c60284b88a32cb396a483675d64076bd527cec1e
jorgy1j <nwajeigeorge7@gmail.com>
2026-09-02 04:25:48 -0700

    Update default values for AWS region and VPC ID

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_region" {
   type   = string
-  default = "us-east-2"
+  default = "us-east-1"
 }
 variable "vpc_id" {
   type   = string
-  default = "vpc-033a164eeac74345f"
+  default = "vpc-07399171ca20f1ca7"
 }
 
 variable "key_name" {
